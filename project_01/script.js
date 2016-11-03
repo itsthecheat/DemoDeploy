91d0d6be1af311521f155b30e0fcbf949253da11
Leslie Behum <llaurenb@gmail.com>
2016-11-03 15:28:46 -0400

    Collision detection working, add animation to cat div

--- project_01/script.js
+++ project_01/script.js
@@ -1,34 +1,70 @@
 "use strict";
-$(document).ready(function () {
+$(document).ready(function() {
+
+    console.log("linked");
+
+    var $dog = $('.dog');
+    var $cat = $('.cat');
+    var body = $('body');
+
+function dogMove() {
+  $(document).mousemove(function(event) {
+        $('.dog').css({
+            'top': event.pageY,
+            'bottom': event.pageX,
+            'left': event.pageX,
+            'right': event.pageY
+        });
+        getCollision();
+    });
+}
+  dogMove();
+    function newCat() {
+        var cat = $('<div class="cat"></div>');
+        body.append(cat);
+        return cat;
+    }
 
 
+    setInterval(function() {
+        $cat.css("top", Math.random() * window.innerHeight);
+        $cat.css("left", Math.random() * window.innerWidth);
+    }, 2000)
 
-console.log("linked");
+    //define function for detectin collision
+    //define variables for width, height and x, y axis of each div
+    function getCollision() {
 
-$(document).mousemove(function(event){
-  $('.dog').css({'top':event.pageY, 'bottom':event.pageX,
-  'left':event.pageX, 'right':event.pageY});
-});
+        var $dogH = $($dog).outerHeight(true);
+        var $dogW = $($dog).outerWidth(true);
+        var $dogX = $($dog).position();
+        var $dogY = $($dog).position();
+        // console.log($dogX)
+        var $dogSideOne = $dogH + $dogY.top;
+        var $dogSideTwo = $dogW + $dogX.left
 
-//define variables for width, height and x, y axis of each div
-var $dog = $('.dog');
-var $cat = $('.cat');
+        var $catH = $($cat).outerHeight(true);
+        var $catW = $($cat).outerWidth(true);
+        var $catX = $($cat).position();
+        var $catY = $($cat).position();
 
-var $dogH = $($dog).outerHeight(true);
-var $dogW = $($dog).outerWidth(true);
-var $dogX = $($dog).position();
-var $dogY = $($dog).position();
-
-var $dogSideOne = $dogH + $dog
-var $dogSideTwo =
-
-var $catH = $($cat).outerHeight(true);
-var $catW = $($cat).outerWidth(true);
-var $catX = $($cat).position();
-var $catY = $($cat).position();
-
-var $catSideOne =
-var $catSideTwo =
+        var $catSideOne = $catH + $catY.top;
+        var $catSideTwo = $catW + $catX.left;
+        // console.log('hhhh');
+        // if ($dogX < $catSideTwo &&
+        //     $dogY < $catSideOne &&
+        //     $catX < $dogSideTwo &&
+        //     $catY < $dogSideOne){
+        //   console.log('boom!');
+        // };
+        if ($dogX.left < $catX.left + $catW &&
+            $dogY.top < $catY.top + $catH &&
+            $catX.left < $dogX.left + $dogW &&
+            $catY.top < $dogY.top + $dogW) {
+            console.log('boom');
+            // $cat.remove();
+        }
+    };
 
 
 
