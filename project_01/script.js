67824cd1e6f730fe18bb0f6b205d1d495506a6f8
Leslie Behum <llaurenb@gmail.com>
2016-11-02 23:06:32 -0400

    Add variables.

--- project_01/script.js
+++ project_01/script.js
@@ -10,10 +10,25 @@ $(document).mousemove(function(event){
   'left':event.pageX, 'right':event.pageY});
 });
 
+//define variables for width, height and x, y axis of each div
+var $dog = $('.dog');
+var $cat = $('.cat');
 
+var $dogH = $($dog).outerHeight(true);
+var $dogW = $($dog).outerWidth(true);
+var $dogX = $($dog).position();
+var $dogY = $($dog).position();
 
+var $dogSideOne = $dogH + $dog
+var $dogSideTwo =
 
+var $catH = $($cat).outerHeight(true);
+var $catW = $($cat).outerWidth(true);
+var $catX = $($cat).position();
+var $catY = $($cat).position();
 
+var $catSideOne =
+var $catSideTwo =
 
 
 
