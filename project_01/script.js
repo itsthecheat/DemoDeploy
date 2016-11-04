5e002acab3706d3381ffe1d4b46578add1d0e722
Leslie Behum <llaurenb@gmail.com>
2016-11-04 15:36:29 -0400

    Add collision on mult cats, cats change class and remove

--- project_01/script.js
+++ project_01/script.js
@@ -3,70 +3,68 @@ $(document).ready(function() {
 
     console.log("linked");
 
-    var $dog = $('.dog');
-    var $cat = $('.cat');
+    var $dog = $('.dog'); //global variables
     var body = $('body');
+    var $cat = $('.cat');
 
-function dogMove() {
-  $(document).mousemove(function(event) {
+
+    function newCat() { //creates a div w/ class cat, appends to body
+        var cat = $('<div class="cat"></div>');
+        body.append(cat);
+        setInterval(function() { //moves cats randomly
+            cat.css("top", Math.random() * window.innerHeight);
+            cat.css("left", Math.random() * window.innerWidth);
+        }, 1500)
+    }
+
+      for(var i=0; i<10; i++) { //create multiple cats
+          newCat();
+      }
+
+    function getCollision(cat) { //collision detection for elements
+        $(cat).each(function(index, cat) {
+            var $dogH = $dog.outerHeight(true);
+            var $dogW = $dog.outerWidth(true);
+            var $dogX = $dog.position();
+            var $dogY = $dog.position();
+
+            var $catH = parseInt($(cat).css('height').replace('px', ''))
+            var $catW = parseInt($(cat).css('width').replace('px', ''))
+            var $catX = parseInt($(cat).css('left').replace('px', ''))
+            var $catY = parseInt($(cat).css('top').replace('px', ''))
+
+
+            if ($dogX.left < $catX + $catW &&
+                $dogY.top < $catY + $catH &&
+                $catX < $dogX.left + $dogW &&
+                $catY < $dogY.top + $dogW) {
+                $(cat).addClass('dead');
+                setTimeout(function() {
+                  $('.dead').remove();
+                }, 2500);
+                $('#score').prepend('<img src="images/cat_dead_sm.jpg">');
+                console.log('boom');
+            }
+        })
+    }
+
+
+    $(document).mousemove(function(event) {
         $('.dog').css({
             'top': event.pageY,
             'bottom': event.pageX,
             'left': event.pageX,
             'right': event.pageY
         });
-        getCollision();
-    });
-}
-  dogMove();
-    function newCat() {
-        var cat = $('<div class="cat"></div>');
-        body.append(cat);
-        return cat;
-    }
+        $cat = $('.cat')
+        getCollision($cat);
+    })();
 
-
-    setInterval(function() {
-        $cat.css("top", Math.random() * window.innerHeight);
-        $cat.css("left", Math.random() * window.innerWidth);
-    }, 2000)
-
-    //define function for detectin collision
-    //define variables for width, height and x, y axis of each div
-    function getCollision() {
-
-        var $dogH = $($dog).outerHeight(true);
-        var $dogW = $($dog).outerWidth(true);
-        var $dogX = $($dog).position();
-        var $dogY = $($dog).position();
-        // console.log($dogX)
-        var $dogSideOne = $dogH + $dogY.top;
-        var $dogSideTwo = $dogW + $dogX.left
-
-        var $catH = $($cat).outerHeight(true);
-        var $catW = $($cat).outerWidth(true);
-        var $catX = $($cat).position();
-        var $catY = $($cat).position();
-
-        var $catSideOne = $catH + $catY.top;
-        var $catSideTwo = $catW + $catX.left;
-        // console.log('hhhh');
-        // if ($dogX < $catSideTwo &&
-        //     $dogY < $catSideOne &&
-        //     $catX < $dogSideTwo &&
-        //     $catY < $dogSideOne){
-        //   console.log('boom!');
-        // };
-        if ($dogX.left < $catX.left + $catW &&
-            $dogY.top < $catY.top + $catH &&
-            $catX.left < $dogX.left + $dogW &&
-            $catY.top < $dogY.top + $dogW) {
-            console.log('boom');
-            // $cat.remove();
-        }
+    function checkWinner() {
+      if ($('.cat').length === 0) {
+        alert("WOOF!")
+      };
     };
-
-
 
 
 
